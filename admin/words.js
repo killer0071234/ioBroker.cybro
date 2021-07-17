edf77906949d5e0acd16fafb9f76539d787a5199
Daniel Gangl <killer007@gmx.at>
2021-07-17 14:39:10 +0200

    added config parameter

--- admin/words.js
+++ admin/words.js
@@ -1,3 +1,9 @@
+/**
+ * @Author: Daniel Gangl
+ * @Date:   2021-07-17 13:26:54
+ * @Last Modified by:   Daniel Gangl
+ * @Last Modified time: 2021-07-17 14:27:39
+ */
 /*global systemDictionary:true */
 "use strict";
 
@@ -14,28 +20,65 @@ systemDictionary = {
 		"pl": "Ustawienia adaptera dla cybro",
 		"zh-cn": "cybro的适配器设置"
 	},
-	"option1": {
-		"en": "option1",
-		"de": "option1",
-		"ru": "option1",
-		"pt": "option1",
-		"nl": "option1",
-		"fr": "option1",
-		"it": "option1",
-		"es": "option1",
-		"pl": "option1",
-		"zh-cn": "option1"
+	"scgiserver": {
+		"en": "Cybrotech scgi server",
+		"de": "Cybrotech scgi server",
+		"ru": "Cybrotech scgi server",
+		"pt": "Cybrotech scgi server",
+		"nl": "Cybrotech scgi server",
+		"fr": "Cybrotech scgi server",
+		"it": "Cybrotech scgi server",
+		"es": "Cybrotech scgi server",
+		"pl": "Cybrotech scgi server",
+		"zh-cn": "Cybrotech scgi server"
 	},
-	"option2": {
-		"en": "option2",
-		"de": "option2",
-		"ru": "option2",
-		"pt": "option2",
-		"nl": "option2",
-		"fr": "option2",
-		"it": "option2",
-		"es": "option2",
-		"pl": "option2",
-		"zh-cn": "option2"
+	"plcnad": {
+		"en": "Cybro-2/3 PLC NAD",
+		"de": "Cybro-2/3 PLC NAD",
+		"ru": "Cybro-2/3 PLC NAD",
+		"pt": "Cybro-2/3 PLC NAD",
+		"nl": "Cybro-2/3 PLC NAD",
+		"fr": "Cybro-2/3 PLC NAD",
+		"it": "Cybro-2/3 PLC NAD",
+		"es": "Cybro-2/3 PLC NAD",
+		"pl": "Cybro-2/3 PLC NAD",
+		"zh-cn": "Cybro-2/3 PLC NAD"
+	},
+	"readInterval": {
+		"en": "read interval in seconds",
+		"de": "read interval in seconds",
+		"ru": "read interval in seconds",
+		"pt": "read interval in seconds",
+		"nl": "read interval in seconds",
+		"fr": "read interval in seconds",
+		"it": "read interval in seconds",
+		"es": "read interval in seconds",
+		"pl": "read interval in seconds",
+		"zh-cn": "read interval in seconds"
+	},
+	"readPlcSysVars": {
+		"en": "read system variables for the PLC",
+		"de": "read system variables for the PLC",
+		"ru": "read system variables for the PLC",
+		"pt": "read system variables for the PLC",
+		"nl": "read system variables for the PLC",
+		"fr": "read system variables for the PLC",
+		"it": "read system variables for the PLC",
+		"es": "read system variables for the PLC",
+		"pl": "read system variables for the PLC",
+		"zh-cn": "read system variables for the PLC"
+	},
+	"readScgiSysVars": {
+		"en": "read system variables for the SCGI server",
+		"de": "read system variables for the SCGI server",
+		"ru": "read system variables for the SCGI server",
+		"pt": "read system variables for the SCGI server",
+		"nl": "read system variables for the SCGI server",
+		"fr": "read system variables for the SCGI server",
+		"it": "read system variables for the SCGI server",
+		"es": "read system variables for the SCGI server",
+		"pl": "read system variables for the SCGI server",
+		"zh-cn": "read system variables for the SCGI server"
 	}
+
 };
