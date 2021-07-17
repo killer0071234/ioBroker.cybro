6a73ab8079a31116635768171063ed03cfdd139a
Daniel Gangl <killer007@gmx.at>
2021-07-17 13:31:54 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"cybro adapter settings": {
+		"en": "Adapter settings for cybro",
+		"de": "Adaptereinstellungen für cybro",
+		"ru": "Настройки адаптера для cybro",
+		"pt": "Configurações do adaptador para cybro",
+		"nl": "Adapterinstellingen voor cybro",
+		"fr": "Paramètres d'adaptateur pour cybro",
+		"it": "Impostazioni dell'adattatore per cybro",
+		"es": "Ajustes del adaptador para cybro",
+		"pl": "Ustawienia adaptera dla cybro",
+		"zh-cn": "cybro的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "option1",
+		"ru": "option1",
+		"pt": "option1",
+		"nl": "option1",
+		"fr": "option1",
+		"it": "option1",
+		"es": "option1",
+		"pl": "option1",
+		"zh-cn": "option1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "option2",
+		"ru": "option2",
+		"pt": "option2",
+		"nl": "option2",
+		"fr": "option2",
+		"it": "option2",
+		"es": "option2",
+		"pl": "option2",
+		"zh-cn": "option2"
+	}
+};
