fa4eca26fb5338635bf73067f2cef16c0206748a
Daniel Gangl <killer007@gmx.at>
2021-07-21 14:29:46 +0200

    [main.js] added callback for written tags

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
  * @Author: Daniel Gangl
  * @Date:   2021-07-17 13:26:54
  * @Last Modified by:   Daniel Gangl
- * @Last Modified time: 2021-07-21 13:57:55
+ * @Last Modified time: 2021-07-21 14:26:17
  */
 "use strict";
 
@@ -33,7 +33,7 @@ class Cybro extends utils.Adapter {
     });
     this.on("ready", this.onReady.bind(this));
     this.on("stateChange", this.onStateChange.bind(this));
-    // this.on("objectChange", this.onObjectChange.bind(this));
+    this.on("objectChange", this.onObjectChange.bind(this));
     // this.on("message", this.onMessage.bind(this));
     this.on("unload", this.onUnload.bind(this));
   }
@@ -71,7 +71,7 @@ class Cybro extends utils.Adapter {
     });
 
     // In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-    this.subscribeStates("testVariable");
+    //this.subscribeStates("testVariable");
     // You can also add a subscription for multiple states. The following line watches all states starting with "lights."
     // this.subscribeStates("lights.*");
     // Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
@@ -145,29 +145,50 @@ class Cybro extends utils.Adapter {
       // clearTimeout(timeout2);
       // ...
       // clearInterval(interval1);
-
+      for (const id in states) {
+        if (!states.hasOwnProperty(id)) continue;
+        this.deletePoll(states[id]);
+      }
       callback();
     } catch (e) {
       callback();
     }
   }
 
-  // If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-  // You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-  // /**
-  //  * Is called if a subscribed object changes
-  //  * @param {string} id
-  //  * @param {ioBroker.Object | null | undefined} obj
-  //  */
-  // onObjectChange(id, obj) {
-  // 	if (obj) {
-  // 		// The object was changed
-  // 		this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-  // 	} else {
-  // 		// The object was deleted
-  // 		this.log.info(`object ${id} deleted`);
-  // 	}
-  // }
+  /**
+   * Is called if a subscribed object changes
+   * @param {string} id
+   * @param {ioBroker.Object | null | undefined} obj
+   */
+  onObjectChange(id, obj) {
+    if (!id) return;
+    if (!obj) {
+      // The object was deleted
+      this.log.info(`object ${id} deleted`);
+      if (states[id]) {
+        deletePoll(states[id]);
+        delete states[id];
+      }
+    } else {
+      // The object was changed
+      if (!obj.native.interval)
+        obj.native.interval = adapter.config.pollInterval;
+      obj.native.interval = parseInt(obj.native.interval, 10);
+      this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
+      if (!states[id]) {
+        states[id] = obj;
+        initPoll(states[id]);
+      } else {
+        if (states[id].native.interval !== obj.native.interval) {
+          deletePoll(states[id]);
+          states[id] = obj;
+          initPoll(states[id]);
+        } else {
+          states[id] = obj;
+        }
+      }
+    }
+  }
 
   /**
    * Is called if a subscribed state changes
@@ -178,6 +199,15 @@ class Cybro extends utils.Adapter {
     if (state) {
       // The state was changed
       this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+      if (states[id].common.write && state.from != "system." + this.namespace) {
+        let writeLink = this.config.scgiServer + "/?" + state.native.link + "=";
+        let wrVal = state.val;
+        if (state.common.type === "boolean") wrVal = wrVal === true ? 1 : 0;
+        writeLink += wrVal;
+        request(writeLink, (error, response, body) => {
+          parseCybroResult(body, this);
+        });
+      }
     } else {
       // The state was deleted
       this.log.info(`state ${id} deleted`);
